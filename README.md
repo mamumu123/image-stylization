9f8f01f038f081731757f1bb746e9bc1d8abee9f
lemma <lemma@tencent.com>
2024-01-27 13:19:23 +0800

    chore: image-stylization

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# image-stylization
-博客：兔年了，利用AI风格化实现剪纸兔、年画兔、烟花兔
+## copy from
+https://github.com/hlwgy/image-stylization
+
+## input source
+https://www.bilibili.com/read/cv17266988/
+
+
+
+## how to use
+
+### install packages
+``` bash
+pip install tensorflow_hub tensorflow pillow
+```
+
+### download models
+
+https://storage.googleapis.com/tfhub-modules/google/magenta/arbitrary-image-stylization-v1-256/2.tar.gz
+
+
+### run
+``` bash
+python python image-stylization.py
+```
--- image-stylization.py
+++ image-stylization.py
@@ -11,8 +11,8 @@ https://storage.googleapis.com/tfhub-modules/google/magenta/arbitrary-image-styl
 https://storage.googleapis.com/tfhub-modules/google/magenta/arbitrary-image-stylization-v1-256/2.tar.gz
 即替换tfhub.dev为storage.googleapis.com/tfhub-modules，并且在末尾加上后缀.tar.gz然后将文件解压到自己喜欢的路径
 """
-hub_model1 = hub.load('image-stylization-v1-256_1')
-hub_model2 = hub.load('image-stylization-v1-256_2')
+hub_model1 = hub.load('magenta_arbitrary-image-stylization-v1-256_1')
+hub_model2 = hub.load('magenta_arbitrary-image-stylization-v1-256_2')
 # %% 
 # tensor转image图片
 def tensor_to_image(tensor):
@@ -39,7 +39,9 @@ def load_img(path_to_img):
 
 # 进行转化
 def stylization(content_path, style_path, mode = "m2"):
+  ## 输入图
   content_image = load_img(content_path)
+  ## 风格图
   style_image = load_img(style_path)
   if mode == "m1":
     stylized_images = hub_model1(tf.constant(content_image), tf.constant(style_image))
@@ -48,24 +50,29 @@ def stylization(content_path, style_path, mode = "m2"):
     stylized_images = hub_model2(tf.constant(content_image), tf.constant(style_image))
     return tensor_to_image(stylized_images[0])
 # %%
-def main():
-
-  mode = "m2"
-  dir_style = "style2"
+def main(content_dir = "input_png", out_dir = 'out', dir_style = 'style', mode = "m2"):
   styles = [f for f in os.listdir(dir_style)]
-  content_dir = "content"
   for style in styles:
     # if style != "bg.png":
     #   continue
     f_names = [f for f in os.listdir(content_dir)]
     for i, n in enumerate(f_names):
-      if n.startswith("_"):
-        continue
-      print(i, "====>", n)
+      # if n.startswith("_"):
+      #   continue
+      
+
+      # print(i, "====>", n)
+      ## 组装数据
       f_path = os.path.join(content_dir , n)
-      img_arr = stylization(f_path, dir_style+"\\"+style, mode)
+      img_path = os.path.join(dir_style, style)
+
+      ## change
+      img_arr = stylization(f_path, img_path, mode)
+
+
       image_pil = PIL.Image.fromarray(img_arr)
-      n_path = os.path.join(content_dir, "_"+style+"+"+mode+"-"+n)
+      n_path = os.path.join(out_dir, "_"+style+"+"+mode+"-"+n)
+      # print('n_path', n_path)
       image_pil.save(n_path)
   print("all completed!")
 
